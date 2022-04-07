42db2680167d33be29c92c8dd5d22bf33c4c3254
Victor Orozco <victor.orozco@accenture.com>
2022-04-07 10:01:56 -0600

    Setup fixed

--- teamRedProject/ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/teamRedProject/components/page/customfooterlibs__002e__html.java
+++ teamRedProject/ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/teamRedProject/components/page/customfooterlibs__002e__html.java
@@ -48,7 +48,7 @@ out.write("\r\n    ");
         }
     }
 }
-out.write("\r\n\r\n\r\n");
+out.write("\r\n\r\n\r\n<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>\r\n");
 
 
 // End Of Main Template Body ----------------------------------------------------------------------
