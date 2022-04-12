013804722c38d2b4584223a78639110350e7eea1
Victor Orozco <victor.orozco@accenture.com>
2022-04-11 20:28:11 -0600

    Card actualizado

--- teamRedProject/ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/teamRedProject/components/card/card__002e__html.java
+++ teamRedProject/ui.apps/target/generated-sources/htl/org/apache/sling/scripting/sightly/apps/teamRedProject/components/card/card__002e__html.java
@@ -38,13 +38,12 @@ Object _global_model = null;
 Object _global_commonstemplates = null;
 Object _global_showcomponent = null;
 Collection var_collectionvar0_list_coerced$ = null;
-Object _dynamic_item = bindings.get("item");
 _global_model = renderContext.call("use", com.teamRedProject.core.models.MultiCardModel.class.getName(), obj());
-out.write("\n    ");
+out.write("\n");
 _global_commonstemplates = renderContext.call("use", "core/wcm/components/commons/v1/templates.html", obj());
 _global_showcomponent = renderContext.getObjectModel().resolveProperty(renderContext.getObjectModel().resolveProperty(_global_model, "cards"), "listChildren");
 if (renderContext.getObjectModel().toBoolean(_global_showcomponent)) {
-    out.write("<div class=\"container\" id=\"header\">\n        <section id=\"card-list-section\" class=\"container\">\n            ");
+    out.write("<section class=\"cmp-resources\" id=\"card-list-section\">\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-md-8\">\n                <div class=\"row\">\n                    <div class=\"col-md-6\">\n                        ");
     {
         Object var_collectionvar0 = renderContext.getObjectModel().resolveProperty(renderContext.getObjectModel().resolveProperty(_global_model, "cards"), "listChildren");
         {
@@ -57,7 +56,7 @@ if (renderContext.getObjectModel().toBoolean(_global_showcomponent)) {
                         {
                             boolean var_validstartstepend6 = (((0 < var_size1) && true) && (var_end5 > 0));
                             if (var_validstartstepend6) {
-                                out.write("<ul style=\"list-style: none;\">");
+                                out.write("<div class=\"cmp-resources__card\">");
                                 if (var_collectionvar0_list_coerced$ == null) {
                                     var_collectionvar0_list_coerced$ = renderContext.getObjectModel().toCollection(var_collectionvar0);
                                 }
@@ -66,46 +65,74 @@ if (renderContext.getObjectModel().toBoolean(_global_showcomponent)) {
                                     {
                                         boolean var_traversal9 = (((var_index7 >= 0) && (var_index7 <= var_end5)) && true);
                                         if (var_traversal9) {
-                                            out.write("\n                <li class=\"cmp-resources__card\">\n                    <p class=\"cmp-resources__card-heading\">");
+                                            out.write("\n                            <p class=\"cmp-resources__card-heading\">");
                                             {
                                                 Object var_10 = renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "title"), "text");
                                                 out.write(renderContext.getObjectModel().toString(var_10));
                                             }
-                                            out.write("</p>\n                    <p class=\"cmp-resources__card-text \">");
+                                            out.write("</p>\n                            <p class=\"cmp-resources__card-text \">");
                                             {
                                                 Object var_11 = renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "description"), "text");
                                                 out.write(renderContext.getObjectModel().toString(var_11));
                                             }
-                                            out.write("</p>\n                    <div><a class=\"btn btn-primary fw-bold cmp-resources__card-btn\"");
+                                            out.write("</p>\n                            <div><a class=\"btn btn-primary fw-bold cmp-resources__card-btn\"");
                                             {
-                                                String var_attrcontent12 = ("#" + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "link"), "uri")));
-                                                out.write(" href=\"");
-                                                out.write(renderContext.getObjectModel().toString(var_attrcontent12));
-                                                out.write("\"");
+                                                Object var_attrvalue12 = renderContext.getObjectModel().resolveProperty(item, "link");
+                                                {
+                                                    Object var_attrcontent13 = renderContext.call("xss", var_attrvalue12, "uri");
+                                                    {
+                                                        boolean var_shoulddisplayattr15 = (((null != var_attrcontent13) && (!"".equals(var_attrcontent13))) && ((!"".equals(var_attrvalue12)) && (!((Object)false).equals(var_attrvalue12))));
+                                                        if (var_shoulddisplayattr15) {
+                                                            out.write(" href");
+                                                            {
+                                                                boolean var_istrueattr14 = (var_attrvalue12.equals(true));
+                                                                if (!var_istrueattr14) {
+                                                                    out.write("=\"");
+                                                                    out.write(renderContext.getObjectModel().toString(var_attrcontent13));
+                                                                    out.write("\"");
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
                                             }
                                             out.write(">");
                                             {
-                                                Object var_13 = renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "buttonText"), "text");
-                                                out.write(renderContext.getObjectModel().toString(var_13));
+                                                Object var_16 = renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "buttonText"), "text");
+                                                out.write(renderContext.getObjectModel().toString(var_16));
                                             }
-                                            out.write("</a></div>\n                    <div><a class=\"cmp-resources__card-link\"");
+                                            out.write("</a></div>\n                            <div><a class=\"cmp-resources__card-link\"");
                                             {
-                                                String var_attrcontent14 = ("#" + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "link"), "uri")));
-                                                out.write(" href=\"");
-                                                out.write(renderContext.getObjectModel().toString(var_attrcontent14));
-                                                out.write("\"");
+                                                Object var_attrvalue17 = renderContext.getObjectModel().resolveProperty(item, "url");
+                                                {
+                                                    Object var_attrcontent18 = renderContext.call("xss", var_attrvalue17, "uri");
+                                                    {
+                                                        boolean var_shoulddisplayattr20 = (((null != var_attrcontent18) && (!"".equals(var_attrcontent18))) && ((!"".equals(var_attrvalue17)) && (!((Object)false).equals(var_attrvalue17))));
+                                                        if (var_shoulddisplayattr20) {
+                                                            out.write(" href");
+                                                            {
+                                                                boolean var_istrueattr19 = (var_attrvalue17.equals(true));
+                                                                if (!var_istrueattr19) {
+                                                                    out.write("=\"");
+                                                                    out.write(renderContext.getObjectModel().toString(var_attrcontent18));
+                                                                    out.write("\"");
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
                                             }
                                             out.write(">");
                                             {
-                                                String var_15 = (renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "buttonText2"), "text")) + " >");
-                                                out.write(renderContext.getObjectModel().toString(var_15));
+                                                String var_21 = (renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(item, "buttonText2"), "text")) + " >");
+                                                out.write(renderContext.getObjectModel().toString(var_21));
                                             }
-                                            out.write("</a></div>\n                </li>\n            ");
+                                            out.write("</a></div>\n                        ");
                                         }
                                     }
                                     var_index7++;
                                 }
-                                out.write("</ul>");
+                                out.write("</div>");
                             }
                         }
                     }
@@ -114,25 +141,19 @@ if (renderContext.getObjectModel().toBoolean(_global_showcomponent)) {
         }
         var_collectionvar0_list_coerced$ = null;
     }
-    out.write("\n\n            ");
-    {
-        String var_16 = (((((((((((((("<!--<div id=\"card-list-section\" class=\"cmp-resources__card\" data-sly-list.item=\"" + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(renderContext.getObjectModel().resolveProperty(_global_model, "cards"), "listChildren"), "comment"))) + "\">\n                <p class = \"cmp-resources__card-heading\" >") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "title"), "comment"))) + "</p>\n                <p class = \"cmp-resources__card-text \">") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "description"), "comment"))) + "</p>\n                    <div><a class=\"btn btn-primary fw-bold cmp-resources__card-btn\" href=\"#") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "link"), "comment"))) + "\">") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "buttonText"), "comment"))) + "</a></div>\n                    <div><a class=\"cmp-resources__card-link\" href=\"#") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "link"), "comment"))) + "\" >") + renderContext.getObjectModel().toString(renderContext.call("xss", renderContext.getObjectModel().resolveProperty(_dynamic_item, "buttonText2"), "comment"))) + " ></a></div>\n            </div>-->");
-        out.write(renderContext.getObjectModel().toString(var_16));
-    }
-    out.write("\n        </section>\n    </div>");
+    out.write("\n                    </div>\n                </div>\n           </div>\n        </div>\n    </div>\n</section>");
 }
 out.write("\n");
 {
-    Object var_templatevar17 = renderContext.getObjectModel().resolveProperty(_global_commonstemplates, "placeholder");
+    Object var_templatevar22 = renderContext.getObjectModel().resolveProperty(_global_commonstemplates, "placeholder");
     {
-        boolean var_templateoptions18_field$_isempty = (!renderContext.getObjectModel().toBoolean(_global_showcomponent));
+        boolean var_templateoptions23_field$_isempty = (!renderContext.getObjectModel().toBoolean(_global_showcomponent));
         {
-            java.util.Map var_templateoptions18 = obj().with("isEmpty", var_templateoptions18_field$_isempty);
-            callUnit(out, renderContext, var_templatevar17, var_templateoptions18);
+            java.util.Map var_templateoptions23 = obj().with("isEmpty", var_templateoptions23_field$_isempty);
+            callUnit(out, renderContext, var_templatevar22, var_templateoptions23);
         }
     }
 }
-out.write("\n\n");
 
 
 // End Of Main Template Body ----------------------------------------------------------------------
